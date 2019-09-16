b90f508c3f956c538edb6f59111eda07fd308408
Charles-IX <rafael.galan.1@ulaval.ca>
2019-09-16 17:35:00 -0400

    Menu V1

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,112 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/album">Album</router-link>
-      </li>
-      <li>
-        <router-link to="/artist">Artist</router-link>
-      </li>
-    </ul>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"><i class="material-icons navbar-icon">home</i>Home</router-link>
+      <a id="hamburger" role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="ubeatNav" v-on:click="hamburgerClick">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+
+    <div id="ubeatNav" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/album" class="navbar-item">
+          <i class="material-icons navbar-icon">album</i>
+          Albums
+        </router-link>
+        <router-link to="/artist" class="navbar-item">
+          <i class="material-icons navbar-icon">person</i>
+          Artists
+        </router-link>
+      </div>
+      <div class="navbar-end">
+
+
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <p id="globalSearchContainer" class="control has-icons-left">
+              <input id="globalSearch" class="input is-small is-rounded" type="text" placeholder="Search..."/>
+              <span class="icon is-small is-left is-black">
+                <i class="material-icons">search</i>
+              </span>
+            </p>
+          </div>
+        </div>
+
+
+        <router-link to="/playlist" class="navbar-item">
+          <i class="material-icons navbar-icon">play_circle_outline</i>
+          Playlists
+        </router-link>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          <i class="material-icons navbar-icon">account_box</i>
+          Rafael Galan
+        </a>
+
+        <div class="navbar-dropdown">
+          <router-link to="/settings" class="navbar-item">
+            <i class="material-icons navbar-icon">build</i>
+            Settings
+          </router-link>
+          <router-link to="/logout" class="navbar-item">
+            <i class="material-icons navbar-icon">lock</i>
+            Logout
+          </router-link>
+        </div>
+      </div>
+      </div>
+    </div>
   </nav>
 </template>
 
-<style></style>
+<style>
+  #globalSearchContainer{
+    width: 100%;
+  }
+
+  .navbar-icon{
+    display: none;
+  }
+
+  @media (min-width: 1025px) {
+    #ubeatNav .navbar-item{
+      padding:15px;
+    }
+
+    .navbar-brand .navbar-item{
+      padding:15px;
+    }
+
+    .navbar-icon{
+      display: inline-block;
+    }
+  }
+</style>
+
+<script>
+export default {
+  methods: {
+    hamburgerClick: function (event){
+        if(event.target.tagName === 'SPAN')
+        {
+          let hamburger = event.target.parentNode;
+        }
+        else
+        {
+          let hamburger = event.target;
+        }
+
+        const target = hamburger.dataset.target;
+        const $target = document.getElementById(target);
+        
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        hamburger.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+    },
+  }
+}
+</script>
