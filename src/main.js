1b1205ad84bfd83b3980daa30f5402c2a1809e7e
Charles-IX <rafael.galan.1@ulaval.ca>
2019-09-22 11:15:02 -0400

    Adding materialize

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "materialize-css";
+import "materialize-css/dist/css/materialize.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
