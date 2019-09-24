7d1f575460ce6a95a5d35436666ec75559e96702
Tsambou Sonkeng Chrispel Simon Francis <chrispel-simon-francis.tsambou-sonkeng.1@ulaval.ca>
2019-09-24 11:20:29 -0400

    Artiste_debut

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
-    <h1>Artist</h1>
-    <div>GLO-3102 Artist page</div>
+    <section class="infoArtist">
+      <img src="https://i1.sndcdn.com/artworks-000087867383-j4snt5-t500x500.jpg" />
+      <h1>Mick Jenkins</h1>
+      <h3>Genre: Jazz</h3>
+      <p><a href="https://www.apple.com/ca/itunes/link/"><img src="https://www.apple.com/ca/itunes/link/images/link-badge-music.png"></a></p>
+    </section>
+    <section>
+      <h3>Liste des albums</h3>
+      <ol class="ListAlbum">
+        <li>
+          <h5>Peaces of a man</h5>
+        </li>
+        <li>
+          <h5>The water(s)</h5>
+        </li>
+        <li>
+          <h5>Wave[s]</h5>
+        </li>
+        <li>
+          <h5>The healing component</h5>
+        </li>
+      </ol>
+
+    </section>
   </div>
 </template>
 
