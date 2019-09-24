85a7f7832037cfa88d3aa2559f0ec33d7cfba4a8
gdesjardins09 <gdesjardins09@gmail.com>
2019-09-24 13:58:28 -0400

    debut css + responsive

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,30 +1,100 @@
 <template>
-  <div>
-    <section class="infoArtist">
-      <img src="https://i1.sndcdn.com/artworks-000087867383-j4snt5-t500x500.jpg" />
-      <h1>Mick Jenkins</h1>
-      <h3>Genre: Jazz</h3>
-      <p><a href="https://www.apple.com/ca/itunes/link/"><img src="https://www.apple.com/ca/itunes/link/images/link-badge-music.png"></a></p>
+  <div class="container">
+    <section class="infoArtist row">
+      <div class="col s12 m6 l6 imgArtist">
+        <img src="https://i1.sndcdn.com/artworks-000087867383-j4snt5-t500x500.jpg" />
+      </div>
+      <div class="col s12 m6 l6">
+        <h2>Mick Jenkins</h2>
+        <h3>Genre: Jazz</h3>
+        <p><a href="https://www.apple.com/ca/itunes/link/"><img src="https://www.apple.com/ca/itunes/link/images/link-badge-music.png"></a></p>
+      </div>
     </section>
     <section>
-      <h3>Liste des albums</h3>
-      <ol class="ListAlbum">
-        <li>
-          <h5>Peaces of a man</h5>
+      <h3 class="listTitle">Albums</h3>
+      <ul class="listAlbum row">
+        <li class="col s6 m4 l2">
+          <a>
+            <img src="https://i1.sndcdn.com/artworks-000428467479-2thnsz-t500x500.jpg">
+          </a>
+          <h3 class="albumTitle">
+            <a>Pieces of a Man</a>
+          </h3>
+          <p>2018</p>
         </li>
-        <li>
-          <h5>The water(s)</h5>
+        <li class="col s6 m4 l2">
+          <a>
+            <img src="https://i1.sndcdn.com/artworks-3e86379b-ae93-443b-887a-52b0a8b05a93-0-t500x500.jpg">
+          </a>
+          <h3 class="albumTitle">
+            <a>Or More The Anxious</a>
+          </h3>
+          <p>2017</p>
         </li>
-        <li>
-          <h5>Wave[s]</h5>
+        <li class="col s6 m4 l2">
+          <a>
+            <img src="https://i1.sndcdn.com/artworks-f12bb816-104e-4655-bc63-6c84c373a7a2-0-t500x500.jpg">
+          </a>
+          <h3 class="albumTitle">
+            <a>The Healing Component</a>
+          </h3>
+          <p>2016</p>
         </li>
-        <li>
-          <h5>The healing component</h5>
+        <li class="col s6 m4 l2">
+          <a>
+            <img src="https://i1.sndcdn.com/artworks-000127868569-v269fn-t500x500.jpg">
+          </a>
+          <h3 class="albumTitle">
+            <a>Wave[s]</a>
+          </h3>
+          <p>2015</p>
         </li>
-      </ol>
+        <li class="col s6 m4 l2">
+          <a>
+            <img src="https://i1.sndcdn.com/artworks-c65a3566-53c4-4cd1-9b24-2a14c90efed4-0-t500x500.jpg">
+          </a>
+          <h3 class="albumTitle">
+            <a>The Water (S)</a>
+          </h3>
+          <p>2014</p>
+        </li>
+      </ul>
 
     </section>
   </div>
 </template>
 
-<style></style>
+<style>
+  .infoArtist {
+    margin-top: 2.37333rem;
+  }
+
+  .imgArtist {
+    display: flex;
+  }
+
+  .imgArtist img {
+    max-width: 340px;
+    width: 100%;
+    margin: auto;
+  }
+
+  .listAlbum a {
+    color: black;
+  }
+
+  .listAlbum a:hover {
+    color: var(--blackLinkHoverColor);
+    cursor: pointer;
+  }
+
+  .listAlbum img {
+    max-width: 100%;
+  }
+
+  .albumTitle {
+    font-size: 14px;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+</style>
