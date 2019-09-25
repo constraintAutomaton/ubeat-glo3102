88264e6a1bc02f4e5c0b08d89ca5791ea60d9980
Bryan <tambryanelliott@gmail.com>
2019-09-25 00:56:53 -0400

    skeleton of album page started, start command added on the package.json

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,7 +1,57 @@
 <template>
   <div>
-    <h1>Album</h1>
-    <div>GLO-3102 Album page</div>
+    <section id="head">
+      <h1 id="album-name">Camelot</h1>
+      <img
+        id="image-album"
+        src="https://i1.sndcdn.com/artworks-000596445971-jh3py0-t500x500.jpg"
+        alt="Album Image"
+      />
+    </section>
+    <section id="track">
+      <ol>
+        <li>
+          <span>button play/pause</span>
+          <span>progess bar</span>
+          <span>track name</span>
+        </li>
+        <li>
+          <span>button play/pause</span>
+          <span>progess bar</span>
+          <span>track name</span>
+        </li>
+        <li>
+          <span>button play/pause</span>
+          <span>progess bar</span>
+          <span>track name</span>
+        </li>
+        <li>
+          <span>button play/pause</span>
+          <span>progess bar</span>
+          <span>track name</span>
+        </li>
+      </ol>
+    </section>
+    <section id="album-info">
+      <ul>
+        <li>
+          <span>Title</span>
+          <span id="title-album">Value</span>
+        </li>
+        <li>
+          <span>release date</span>
+          <span id="release-date-album">Value</span>
+        </li>
+        <li>
+          <span>Number of track</span>
+          <span id="number-track-album">Value</span>
+        </li>
+        <li>
+          <span>Gender</span>
+          <span id="gender-album">Value</span>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
