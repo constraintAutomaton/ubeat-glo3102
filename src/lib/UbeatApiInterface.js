4167e62e035cdf11a4c31e1aaa55dfeaa553f0a9
Bryan <tambryanelliott@gmail.com>
2019-10-29 06:42:41 -0400

    api call for the artist and album page done but not tested starting the util functions for the album page

--- src/lib/UbeatApiInterface.js
+++ src/lib/UbeatApiInterface.js
@@ -1,3 +1,8 @@
+/**
+ * This class handle the raw request to the Ubeat Api
+ * Futher threatement are in the util class corresponding to the component.
+ */
+
 export default class UbeatApiInterface {
   constructor(isSecure = true) {
     this.rootUrl = "https://ubeat.herokuapp.com/";
@@ -14,7 +19,7 @@ export default class UbeatApiInterface {
       }
     };
     p_query = p_query.replace(new RegExp(" ", "g"), "%20");
-    let rep = await fetch(
+    const rep = await fetch(
       `${this.rootUrl}search/${p_type}?q=${p_query}&limit=${p_limite}`,
       param
     );
@@ -30,5 +35,35 @@ export default class UbeatApiInterface {
   }
   async searchTracks(p_query, p_limite = 10) {
     return await this.search(p_query, "tracks", p_limite);
+  }
+  async _getAlbumOrArtist(p_type, p_id, p_getTracksOrAlbum = "") {
+    const param = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    };
+    p_getTracksOrAlbum =
+      p_getTracksOrAlbum != "" ? p_getTracksOrAlbum + "/" : "";
+    const rep = await fetch(
+      `${this.rootUrl}${p_type}/${p_id}${p_getTracksOrAlbum}`,
+      param
+    );
+    if (rep.ok) {
+      return await rep.json();
+    }
+  }
+  async getAlbumById(p_id) {
+    return await this._getAlbumOrArtist("albums", p_id);
+  }
+  async getAlbumTrackById(p_id) {
+    return await this._getAlbumOrArtist("albums", p_id, "tracks");
+  }
+  async getArtistById(p_id) {
+    return await this._getAlbumOrArtist("artists", p_id);
+  }
+  async getArtistAlbumById(p_id) {
+    return await this._getAlbumOrArtist("artists", p_id, "albums");
   }
 }
