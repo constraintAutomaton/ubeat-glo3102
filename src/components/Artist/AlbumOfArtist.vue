8a35e25dc1b4fc125c18530dde7946aa6c9439d9
Tsambou Sonkeng Chrispel Simon Francis <chrispel-simon-francis.tsambou-sonkeng.1@ulaval.ca>
2019-10-31 13:24:06 -0400

    Artist component continuation

---
+++ src/components/Artist/AlbumOfArtist.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "AlbumOfArtist"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Artist/AlbumsItem.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "AlbumsItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
