a6fa254ec2fc58966a263a257bf6d80f6782505a
Tsambou Sonkeng Chrispel Simon Francis <chrispel-simon-francis.tsambou-sonkeng.1@ulaval.ca>
2019-11-06 15:26:57 -0500

    mise a jour  component Artist 2

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -6,6 +6,49 @@
       v-bind:genre="artistInfo.genre"
       v-bind:artistLink="artistInfo.artistLink"
     ></ArtistInfo>
+    <section>
+      <h3 class="listTitle">Latest Releases</h3>
+      <ul class="latestReleases row">
+        <li class="col s12 m6 l6">
+          <div class="release flexContent">
+            <img
+              src="https://i1.sndcdn.com/artworks-000428467479-2thnsz-t500x500.jpg"
+              alt="Album Image"
+              class="trackImg"
+            />
+            <img
+              src="https://www.ccfinc.ca/wp-content/uploads/play-button.png"
+              class="playButton"
+            />
+            <div class="infoTrack">
+              <p class="trackPosition">
+                <span class="trackName">Track Name</span>
+              </p>
+            </div>
+          </div>
+        </li>
+        <li class="col s12 m6 l6">
+          <div class="release flexContent">
+            <img
+              src="https://i1.sndcdn.com/artworks-000428467479-2thnsz-t500x500.jpg"
+              alt="Album Image"
+              class="trackImg"
+            />
+            <img
+              src="https://www.ccfinc.ca/wp-content/uploads/play-button.png"
+              class="playButton"
+            />
+            <div class="infoTrack">
+              <p class="trackPosition">
+                <span class="trackName">Track Name</span>
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <Tracks title="Popular tracks"></Tracks>
 
     <AlbumOfArtist title="Albums"
                    v-bind:albumList="albumInfo"
@@ -47,7 +90,8 @@
       },
       components: {
           ArtistInfo,
-          AlbumOfArtist
+          AlbumOfArtist,
+          Tracks
       }
 
   };
