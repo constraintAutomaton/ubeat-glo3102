13bb36378a94d7605d8417227aa16427810c6597
Tsambou Sonkeng Chrispel Simon Francis <chrispel-simon-francis.tsambou-sonkeng.1@ulaval.ca>
2019-11-11 00:20:59 -0500

    utilArtist.js legerement modifié

--- src/lib/util/utilArtist.js
+++ src/lib/util/utilArtist.js
@@ -8,14 +8,14 @@ export const getArtistInfo = async p_artist => {
   const result = searchResults.results[0];
   let formated = {
     artistName: result.artistName,
-    imgArtist: result.achercher,
+    //imgArtist: result.achercher,
     genre: result.primaryGenreName,
     artistLink: result.artistLinkUrl
   };
   const highResImage = await apiEngine.getHighResImage(
-    p_artist,
-    "artiste",
-    p_artist
+    //p_artist,
+    //"artiste",
+    //p_artist
   );
   formated["imgArtist"] = highResImage != {} ? highResImage : "";
 
@@ -30,18 +30,19 @@ export const getAlbumInfo = async (p_id, nb_album = -1) => {
   const formated = results.map(el => {
     return {
       albumTitle: el.collectionName,
+      albumImage: el.artworkUrl100,
       albumYear: new Date(el.releaseDate).getFullYear()
     };
   });
-  for (let i = 0; i < formated.length; i++) {
-    let highResImage = await apiEngine.getHighResImage(
-      results[i].collectionName,
-      "album",
-      results[i].artistName
-    );
-    highResImage = highResImage != {} ? highResImage : "";
-    formated[i]["albumImage"] = await highResImage;
-  }
+  //for (let i = 0; i < formated.length; i++) {
+    //let highResImage = await apiEngine.getHighResImage(
+      //results[i].collectionName,
+      //"album",
+     // results[i].artistName
+    //);
+    //highResImage = highResImage != {} ? highResImage : "";
+   // formated[i]["albumImage"] = await highResImage;
+  //}
   console.log(formated);
   return formated;
 };
