77c5a3710e069aa0120e738c43c72b3074d8d940
Charles-IX <rafael.galan.1@ulaval.ca>
2019-11-16 01:43:32 -0500

    Bug fix

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -41,6 +41,9 @@ export default {
         const album = await getAlbumById(this.$route.params.id);
         this.albumInfo = album;
       }
+      else {
+        this.albumInfo = null;
+      }
     }
   },
   components: {
