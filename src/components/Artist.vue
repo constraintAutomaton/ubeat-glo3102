6b4572f9274b78b60ba3c0ca9532c89aeba8c3bd
Bryan <tambryanelliott@gmail.com>
2019-12-14 08:27:10 -0500

    bio added to artist page

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="artistPage" class="container">
-    <div v-if="loading">
-      Loading...
-    </div>
+    <div v-if="loading">Loading...</div>
     <div v-if="artistInfo">
       <ArtistInfo
         :artistName="artistInfo.artistName"
@@ -10,27 +8,30 @@
         :genre="artistInfo.primaryGenreName"
         :artistLink="artistInfo.artistLinkUrl"
       ></ArtistInfo>
-           <AlbumOfArtist
-        title="Albums"
-        :albumList="artistInfo.albums"
-      ></AlbumOfArtist>
+      <h1>Biographie</h1>
+      <p v-if!="loading">
+        {{showAllBio===false?artistInfo.bio.substring(0,maxStringBio):artistInfo.bio}}
+        <span v-if="artistInfo.bio.length===0 || showAllBio===false">[...]</span>
+      </p>
+      <button v-on:click="showBio" v-if!="artistInfo.bio.length===0">Show more</button>
+      <AlbumOfArtist title="Albums" :albumList="artistInfo.albums"></AlbumOfArtist>
     </div>
-    <div v-if="error">
-      {{ error }}
-    </div>
+    <div v-if="error">{{ error }}</div>
   </div>
 </template>
 <script>
 import ArtistInfo from "./Artist/ArtistInfo.vue";
 import AlbumOfArtist from "./Artist/AlbumOfArtist.vue";
-import { getArtistById,getAlbumOfArtist } from "../lib/util/utilArtist";
+import { getArtistById, getAlbumOfArtist } from "../lib/util/utilArtist";
 
 export default {
   data() {
     return {
       artistInfo: null,
       loading: true,
-      error: null
+      error: null,
+      maxStringBio: 1100,
+      showAllBio: false
     };
   },
   watch: {
@@ -42,11 +43,12 @@ export default {
   methods: {
     async loadArtist() {
       this.error = this.artistInfo = null;
-      this.loading = false;
+      this.loading = true;
 
       try {
         let artist = await getArtistById(this.$route.params.id);
         artist.albums = await getAlbumOfArtist(this.$route.params.id);
+        console.log(artist);
         this.artistInfo = artist;
         this.loading = false;
         this.error = false;
@@ -54,11 +56,14 @@ export default {
         this.error = error;
         this.loading = false;
       }
+    },
+    showBio() {
+      this.showAllBio = !this.showAllBio;
     }
   },
   components: {
     ArtistInfo,
-    AlbumOfArtist,
+    AlbumOfArtist
   }
 };
 </script>
