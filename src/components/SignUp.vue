348482000ad6d1635057afe298b2ec81fecce482
Bryan <tambryanelliott@gmail.com>
2019-12-14 19:39:15 -0500

    small validation simplification

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="signupPage" class="container">
     <div id="signup-form">
-      <h3>Signup</h3><div class="row">
-      <div class="input-field col s12">
-        <input id="name" type="text" v-model="inputName" class="validate">
-        <label for="name">Enter your username</label>
-      </div>
-    </div>
+      <h3>Signup</h3>
       <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" v-model="inputEmail" class="validate" required>
+          <input id="name" type="text" v-model="inputName" class="validate" />
+          <label for="name">Enter your username</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" v-model="inputEmail" class="validate" required />
           <label for="email">Email</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="password" type="password" v-model="inputPassword" data-length="10" class="validate">
+          <input
+            id="password"
+            type="password"
+            v-model="inputPassword"
+            data-length="10"
+            class="validate"
+          />
           <label for="password">Password</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="inputRepeatPassword" type="password" v-model="inputRepeatPassword" class="validate">
+          <input
+            id="inputRepeatPassword"
+            type="password"
+            v-model="inputRepeatPassword"
+            class="validate"
+          />
           <label for="password">Repeat password</label>
         </div>
       </div>
-      <div >
-        <button class="btn waves-effect waves-light" type="submit" name="signup" v-on:click="signup">SIGNUP</button>
+      <div>
+        <button
+          class="btn waves-effect waves-light"
+          type="submit"
+          name="signup"
+          v-on:click="signup"
+        >SIGNUP</button>
       </div>
     </div>
   </div>
@@ -34,62 +51,58 @@
 
 <script>
 import ApiInterface from "./../lib/ApiInterface";
-import { required, email } from "vuelidate/lib/validators";
-    export default {
-        data(){
-            return {
-                inputName: '',
-                inputEmail: '',
-                inputPassword: '',
-                inputRepeatPassword: ''
-            };
-        },
-        validations() {
-            return {
-                inputName:{
-                    required
-                },
-                inputEmail: {
-                    required,
-                    email
-                },
-                inputPassword: {
-                    required
-                },
-                inputRepeatPassword: {
-                    required
-                }
-            };
-        },
-        methods: {
-            signup: async function(event){
-                    console.log(event);
-                    if (this.$v.inputName.required === true &&
-                        this.$v.inputPassword.required === true &&
-                        this.$v.inputRepeatPassword.required === true &&
-                        this.$v.inputEmail.required === true &&
-                        this.$v.inputEmail.email === true) {
-                        if(this.inputPassword===this.inputRepeatPassword){
-                            const engine =new ApiInterface(false);
-                            const rep = await engine.signup(this.inputName, this.inputEmail, this.inputPassword)
-                            console.log(rep);
-                            M.toast({html: 'Successful sign up, welcome', classes: 'rounded'});
-                            setTimeout(()=>{
-                               window.location.hash='/login';
-                            },2000);
+import { required, email, sameAs } from "vuelidate/lib/validators";
+export default {
+  data() {
+    return {
+      inputName: "",
+      inputEmail: "",
+      inputPassword: "",
+      inputRepeatPassword: ""
+    };
+  },
+  validations() {
+    return {
+      inputName: {
+        required
+      },
+      inputEmail: {
+        required,
+        email
+      },
+      inputPassword: {
+        required
+      },
+      inputRepeatPassword: {
+        required,
+        sameAsPassword: sameAs("inputPassword")
+      }
+    };
+  },
+  methods: {
+    signup: async function(event) {
+      console.log(event);
+      if (!this.$v.$invalid) {
+        const engine = new ApiInterface(false);
+        const rep = await engine.signup(
+          this.inputName,
+          this.inputEmail,
+          this.inputPassword
+        );
 
-                        } else {
-                            M.toast({html: 'Passwords do not match', classes: 'rounded'});
-                        }
-                    } else {
-                        M.toast({
-                            html: "Username, email, password or repeat Password missing",
-                            classes: "rounded"
-                        });
-                    }
-            }
+        M.toast({ html: "Successful sign up, welcome", classes: "rounded" });
+        setTimeout(() => {
+          window.location.hash = "/login";
+        }, 2000);
+      } else {
+        M.toast({
+          html: "Username, email, password or repeat Password missing or wrong",
+          classes: "rounded"
+        });
+      }
     }
-    }
+  }
+};
 </script>
 
 <style scoped>
@@ -97,11 +110,10 @@ import { required, email } from "vuelidate/lib/validators";
   height: 35px;
   background-color: #2ab7a9;
 }
-  #signup-form{
-    width: 50vw;
-    display: flex;
-    flex-direction: column;
-    justify-items:center;
-  }
-
+#signup-form {
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+}
 </style>
